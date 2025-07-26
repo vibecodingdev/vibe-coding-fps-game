32aa3e490c07bff75235347efe88da8fa6453159
glit <hi@glit.ltd>
2025-07-27 00:29:25 +0800

    fix pm2

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "doom-server",
-      script: "dist/index.js",
+      script: "dist/src/index.js",
       instances: 1,
       exec_mode: "fork",
       watch: false,
